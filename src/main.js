294c9d1224702b46a7d3826764218862a11abfcc
cuizhenwei <cui284934@sina.com>
2020-08-21 19:19:56 +0800

    node-server

--- src/main.js
+++ src/main.js
@@ -3,8 +3,15 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import Axios from 'axios'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import locale from 'element-ui/lib/locale/lang/en' // 国际化
 
 Vue.config.productionTip = false
+
+Vue.use(ElementUI, { locale });
+Vue.prototype.$http= Axios
 
 new Vue({
   router,
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,23 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+     <el-button type="primary" @click="getUserList" size="mini">查看用户</el-button>
+    {{userData}}
   </div>
 </template>
+<script>
+export default {
+  data(){
+    return {
+      userData:[]
+    }
+  },
+  methods:{
+    getUserList(){
+      this.$http.get('http://localhost:5000/users/getUsers').then(res => {
+        this.userData = res.data
+      })
+    }
+  }
+}
+</script>
